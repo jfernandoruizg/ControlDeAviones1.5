8fcd646ce7b6309db9de0d86046b8e9d5c03bdc8
jfernandoruiz <ferb2013ruiz@gmail.com>
2017-08-28 12:14:55 -0600

    change commit 2

---
+++ app/views/avions/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.partial! "avions/avion", avion: @avion
---
+++ test/system/avions_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class AvionsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit avions_url
+  #
+  #   assert_selector "h1", text: "Avion"
+  # end
+end
